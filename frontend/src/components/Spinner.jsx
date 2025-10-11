3313a6681dc709a1eb5b9ed37d4ee9ca50456f6d
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-10-11 11:22:57 +0200

    Added react-spinners

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,9 +1,20 @@
-function Spinner() {
-  return (
-    <div className='loadingSpinnerContainer'>
-      <div className='loadingSpinner'></div>
-    </div>
-  )
+import { ClipLoader } from 'react-spinners';
+
+const Spinner = () => {
+
+    const override = {
+        display: 'block',
+        margin: '20% auto',
+        color: 'blue',
+        text: '50px',
+    }
+
+    return (
+        <ClipLoader
+        cssOverride={override}
+        size={150}
+        />
+    )
 }
 
 export default Spinner

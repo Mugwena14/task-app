f40534451ed81f22b3a8d65546adc5e47d99e23e
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-10-15 13:24:17 +0200

    Added the summarize route

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -5,11 +5,18 @@ const {
     createNote,
     updateNote,
     deleteNote,
+    summarizeNote, 
 } = require('../controllers/noteController')
 
 const { protect } = require('../middleware/authMiddleware')
 
+// Get & Create Notes
 router.route('/').get(protect, getNotes).post(protect, createNote)
+
+// Update & Delete Notes
 router.route('/:id').put(protect, updateNote).delete(protect, deleteNote)
+
+// Summarize a Note
+router.route('/:id/summarize').post(protect, summarizeNote) 
 
 module.exports = router
